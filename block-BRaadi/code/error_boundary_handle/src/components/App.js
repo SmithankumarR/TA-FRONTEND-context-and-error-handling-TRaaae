843d41332bf7ebcb417cbd406dca37e973d6f442
SmithankumarR <smithankumar404@gmail.com>
2022-12-30 14:04:50 +0530

    handle_error_boundaries

--- block-BRaadi/code/error_boundary_handle/src/components/App.js
+++ block-BRaadi/code/error_boundary_handle/src/components/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
-
+import ErrorBoundary from "./ErrorBoundaries";
 class App extends React.Component {
+  state ={
+    isDarkMode : true
+  }
   render() {
     let { isDarkMode } = this.state;
     return (
-      <>
+      <ErrorBoundary>
         <Header isDarkMode={isDarkMode} />
         <Main isDarkMode={isDarkMode} />
-      </>
+      </ErrorBoundary>
     );
   }
 }
